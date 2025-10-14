7e1f2cae31f079565cce17a69a037484f9de3cde
Sergey Korsensky <sergey.korsensky@intel.com>
2025-10-14 17:02:28 +0300

    Add Header component and show the cars list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,42 @@
-import './App.css'
+import Header from "./components/Header.jsx";
+import './App.css';
 
 export default function App() {
-  return (
+      const data = [{
+            id: 1,
+            name: "Fiat"
+        },
+        {
+            id: 2,
+            name: "Peugeot"
+        },
+        {
+            id: 3,
+            name: "Ford"
+        },
+        {
+            id: 4,
+            name: "Renault"
+        },
+        {
+            id: 5,
+            name: "Citroen"
+        }
+    ]
+    return (
     <div className="bg-purple-800 text-white min-h-screen p-4 flex flex-col justify-center  items-center">
-      <h1 className="text-3xl font-thin">
-        Hello FARM stack!!!
-      </h1>
+      <Header/>
+      <div className="grid grid-cols-1 gap-4">
+          {data.map((el) => {
+              return (
+                  <div key={el.id}>
+                      Cars listed as{"  "}
+                      <span className="font-bold">{el.name.toUpperCase()}</span>
+                  </div>
+              )
+          }
+          )}
+      </div>
     </div>
   )
 }
