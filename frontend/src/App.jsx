be8a42de45f7c606b2b3e1c639c7fcab91b52fbb
Sergey Korsensky <sergey.korsensky@intel.com>
2025-10-15 20:47:16 +0300

    Use the "useEffect" hook to fetch external data using API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,31 @@
 import Card from "./components/Card.jsx";
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect } from "react";
 
 export default function App() {
-    const data = [
-        { id: 1, name: "Fiat", year: 2023, model: "Panda", price: 12000 },
-        { id: 2, name: "Peugeot", year: 2018, model: "308", price: 16000 },
-        { id: 3, name: "Ford", year: 2022, model: "Mustang", price: 25000 },
-        { id: 4, name: "Renault", year: 2019, model: "Clio", price: 18000 },
-        { id: 5, name: "Citroen", year: 2021, model: "C3 Aircross", price: 22000 },
-        { id: 6, name: "Toyota", year: 2020, model: "Yaris", price: 15000 },
-        { id: 7, name: "Volkswagen", year: 2021, model: "Golf", price: 28000 },
-        { id: 8, name: "BMW", year: 2022, model: "M3", price: 45000 },
-        { id: 9, name: "Mercedes", year: 2021, model: "A-Class", price: 35000 },
-        { id: 10, name: "Audi", year: 2022, model: "A6", price: 40000 }
-    ]
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
-    const [budget, setBudget] = useState(20000);
-
+    const fetchUsers = () => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }
     return (
-    <div className="bg-purple-800 text-white min-h-screen p-4 flex flex-col justify-center  items-center">
-      <div>
-          <h2>Your budget is {budget}</h2>
-          <label htmlFor="budget">Budget : </label>
-          <input type="number" className="text-black" step={1000} id="budget" value={budget} onChange={(e) => setBudget(e.target.value)} />
-      </div>
-      <div className="grid grid-cols-1 gap-4">
-          {data.filter((el) => el.price <= budget).map((el) => {
-              return (
-                  <Card car={el} key={el.id} />
-              )
-          }
-          )}
-      </div>
-    </div>
-  )
+        <div className="bg-purple-800 text-white min-h-screen p-4 flex flex-col justify-center  items-center">
+            <h2 className="mb-4">List of users</h2>
+            <div className="grid grid-cols-1 gap-4">
+                <ol>
+                    {
+                        users.map((user) => (
+                                 <li key={user.id}>{user.name}</li>
+                            )
+                        )
+                    }
+                </ol>
+            </div>
+        </div>
+    )
 }
